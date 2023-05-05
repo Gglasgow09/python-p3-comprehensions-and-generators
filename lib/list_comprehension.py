00a4edac2adc6b6289b33ce5f5f3d578b31d7b3f
Gabrielle Glasgow <g.glasgow91@gmail.com>
2023-05-04 21:29:40 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    
+    # Use a list comprehension to filter out odd elements and keep even ones.
+    even_nums = [num for num in num_list if num % 2 == 0]
+
+    return even_nums
+
 
 def make_exclamation(sentence_list):
-    pass
+    add_exclamation = [ statement + "!" for statement in sentence_list ]
+
+    return add_exclamation
